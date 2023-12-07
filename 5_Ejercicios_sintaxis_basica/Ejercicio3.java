a44f76986a5559bb084abd61ae4bfb24333efe7d
CristianInBits <cl.sindila@gmail.com>
2023-12-08 00:20:53 +0100

    5_Ejercicio3

---
+++ 5_Ejercicios_sintaxis_basica/Ejercicio3.java
@@ -0,0 +1,14 @@
+/**Modificar el programa del ejercicio uno para que salgan por pantalla los
+ * números del cero al diez, mostrando si cada uno de ellos es par o impar.
+ */
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            if (i % 2 == 0)
+                System.out.println(i + " es par");
+            else
+                System.out.println(i + " es impar");
+        }
+    }
+}
