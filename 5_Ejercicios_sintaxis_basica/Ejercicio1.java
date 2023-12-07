f54e2ecf916c652d189f2d684cd5421d56d733e8
CristianInBits <cl.sindila@gmail.com>
2023-12-08 00:06:19 +0100

    5_Ejercicio1

---
+++ 5_Ejercicios_sintaxis_basica/Ejercicio1.java
@@ -0,0 +1,14 @@
+/**
+ * Realizar un programa que muestre por pantalla los números del cero al diez
+ * utilizando un bucle for.
+ */
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
