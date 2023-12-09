bce7e4c00708eb8281cffdb76ef72c6767c8a92b
CristianInBits <cl.sindila@gmail.com>
2023-12-09 18:40:56 +0100

    5_Ejercicio6

---
+++ 5_Ejercicios_sintaxis_basica/Ejercicio6.java
@@ -0,0 +1,13 @@
+/**
+ * Calcular el factorial del número siete, usando WHILE, DO...WHILE y FOR.
+ */
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 10);
+    }
+}
