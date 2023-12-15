52fa958aa8a22e0ba98a6299af7d42555f2cf279
CristianInBits <cl.sindila@gmail.com>
2023-12-15 10:25:05 +0100

    7_Ejercicio6

---
+++ 7_Ejercicios_sintaxis_basica_extra/Ejercicio6.java
@@ -0,0 +1,23 @@
+/**
+ * Realizar un programa que muestre por pantalla la siguiente pirámide:
+ * 1
+ * 22
+ * 333
+ * 4444
+ * 55555
+ */
+
+ /**
+  * Ejercicio6
+  */
+ public class Ejercicio6 {
+ 
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+ }
