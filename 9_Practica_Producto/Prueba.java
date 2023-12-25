3c7c5d6eafe1bbb1c8d737679dcb77abe40ce1b3
CristianInBits <cl.sindila@gmail.com>
2023-12-25 17:39:35 +0100

    Producto_prueba

--- 9_Practica_Producto/Prueba.java
+++ 9_Practica_Producto/Prueba.java
@@ -1,3 +1,37 @@
 /**
  * CLASE “PRUEBA” QUE EN EL “MAIN” UTILICE TODOS LOS MÉTODOS
  */
+public class Prueba {
+
+    public static void main(String[] args) {
+    
+    // Crear un objeto Producto:
+    Producto producto1 = new Producto("chocolatinas",30,2);
+
+    // Imprimir los datos iniciales del producto:
+    producto1.imprimir();
+
+    // Vender producto
+    System.out.println("Se vendió chocolatinas por un precio de " + producto1.venderProducto(5) + " euros");
+    producto1.imprimir();
+
+    // Producto por defecto
+    Producto producto2 = new Producto();
+
+    // Producto 3
+    Producto producto3 = new Producto("pan", 10, 0.5);
+
+    // RESUMEN
+    producto1.imprimir();
+    producto2.imprimir();
+    producto3.imprimir();
+    
+    //Añadir más pan
+    producto3.reabastecerInventario(13);
+    // RESUMEN
+    producto1.imprimir();
+    producto2.imprimir();
+    producto3.imprimir();
+}
+
+}
