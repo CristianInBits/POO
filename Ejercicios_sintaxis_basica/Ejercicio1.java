83182f5ea781a310938242cfb61d40d4cf9c61cd
CristianInBits <128726794+CristianInBits@users.noreply.github.com>
2024-06-04 23:47:24 +0200

    Ejercicios_sintaxis_basica_Ejercicio1

---
+++ Ejercicios_sintaxis_basica/Ejercicio1.java
@@ -0,0 +1,9 @@
+package Ejercicios_sintaxis_basica;
+/* Realizar un programa que muestre por pantalla los números del cero al diez utilizando un bucle for. */
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++)
+            System.out.println(i);
+    }
+}
