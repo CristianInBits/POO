a088c71e3e5aec613d00c3d137cb01d4eb133c48
CristianInBits <128726794+CristianInBits@users.noreply.github.com>
2024-06-06 17:49:48 +0200

    Ejercicios_sintaxis_basica_Ejercicio5

---
+++ Ejercicios_sintaxis_basica/Ejercicio5.java
@@ -0,0 +1,9 @@
+/* Modificar el programa del ejercicio uno para mostrar los números usando una instrucción WHILE*/
+public class Ejercicio5 {
+    public static void main (String [] args){
+		int i=0; 
+		while (i<=10){
+			System.out.println (i); i++; 
+		} 
+	} 
+}
